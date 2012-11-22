1bcf06da7889d929d73533ca5b7eaacc68613963
Michael Mullins <mcmullins@mail.umhb.edu>
2012-11-22 00:25:01 -0600

    Lazy::File#open

--- lib/lazy_files/files/file.rb
+++ lib/lazy_files/files/file.rb
@@ -2,7 +2,7 @@ require 'lazy_files/files/file_mods'
 require 'lazy_files/files/constants'
 
 class Lazy::File
-  attr_reader :path
+  attr_reader :path, :io
 
   PATHBASED_METHODS.each do |method_name|
     define_method(method_name) do
@@ -12,26 +12,36 @@ class Lazy::File
 
   def initialize(path, *args, &block)
     @path = File.expand_path path
+    @io = nil
     unless File.exists? @path
       args = ['w'] if args.empty?
       if block_given?
         File.open(@path, *args, &block)
       else
-        File.open(@path, *args) {}
+        File.open(@path, *args).close
       end
     end
   end
 
-  def basename
-    File.basename @path
+  def basename(with_ext = true)
+    name = File.basename @path
+    if with_ext
+      name
+    else
+      name[/.*(?=#{extname}\Z)/]
+    end
   end
 
   def name
-    basename[/.*(?=#{extname}\Z)/]
+    basename(false)
   end
 
   def to_s
     "#<LazyFile:#{basename}>"
+  end
+
+  def open(*args, &block)
+    @io = File.open(@path, *args, &block)
   end
 
 end
--- spec/file_spec.rb
+++ spec/file_spec.rb
@@ -5,36 +5,71 @@ describe Lazy::File do
   before(:all) { Dir.chdir TESTDIR }
   after(:each) { Spwn.clean TESTDIR }
   after(:all) { Dir.chdir ROOT}
-  subject { Lazy.file('test.md') }
+  subject(:file) { Lazy.file('hello.txt') }
+
+  its(:io) { should be_nil }
+  its(:path) { should == File.join(TESTDIR, 'hello.txt')}
 
   it { should respond_to :basename }
-  context "#exist?" do
-    it "should respond to all path based File methods" do
+
+  context '#basename' do
+    it "should return the file's current basename" do
+      file.basename.should == 'hello.txt'
+    end
+    context 'when the with_ext option is false' do
+      it 'it should return the name without an extension' do
+        file.basename(false).should == 'hello'
+      end
+      it 'should return the files name' do
+        Lazy.file('hello.jpg').basename(false).should == 'hello'
+      end
+      it 'should work with dotfiles' do
+        Lazy.file('.gitignore').basename(false).should == '.gitignore'
+      end
+      it 'should work with multiple extensions' do
+        Lazy.file('main.js.coffee').basename(false).should == 'main.js'
+      end
+    end
+  end
+
+  context '#name' do
+    it 'should return the basename without an extension' do
+      file.name.should == 'hello'
+    end
+  end
+
+  context '#exist?' do
+    it 'should respond to all path based File methods' do
       Lazy::File::PATHBASED_METHODS.each do |method_name|
         should respond_to method_name
       end
     end
-    it "should be true if the file exists" do
-      file = Lazy.file('hello.jpg')
+    it 'should be true if the file exists' do
       file.should exist
     end
-    it "should be false if the file does not exist" do
-      file = Lazy.file('hello.jpg')
+    it 'should be false if the file does not exist' do
       File.delete(file.path)
       file.should_not exist
     end
   end
-  context "#name" do
-    it "should return the files name" do
-      Lazy.file('hello.jpg').name.should == "hello"
+
+  context '#open' do
+    subject(:file) { Lazy.file('hello.txt') }
+    it 'should allow you to open an access a file' do
+      file.open('w') do |f|
+        f.puts 'hello world'
+      end
+      `cat hello.txt`.strip.should == 'hello world'
     end
-    it "should work with dotfiles" do
-      Lazy.file('.gitignore').name.should == ".gitignore"
+    it "should set an io value if a block isn't given" do
+      file.open('w')
+      file.io.should be_an_instance_of File
     end
-    it "should work with multiple extensions" do
-      Lazy.file('main.js.coffee').name.should == "main.js"
+    it 'should return a File object' do
+      file.open('w').should be_an_instance_of File
     end
   end
+
 end
 
 describe Lazy do
@@ -43,29 +78,29 @@ describe Lazy do
   after(:all) { Dir.chdir ROOT}
   context "::basename" do
     it { should respond_to :basename }
-    it "should accept a path" do
-      Lazy.basename('hello/testfile.jpeg').should == "testfile.jpeg"
+    it 'should accept a path' do
+      Lazy.basename('hello/testfile.jpeg').should == 'testfile.jpeg'
     end
-    it "should accept a Lazy::File" do
+    it 'should accept a Lazy::File' do
       Spwn.file('testfile.jpeg')
       file = Lazy.file('testfile.jpeg')
       Lazy.basename(file)
     end
   end
 
-  context "::file" do
+  context '::file' do
     it { should respond_to :file}
-    it "should create a file if it does not exist" do
+    it 'should create a file if it does not exist' do
       Lazy.file('testfile.jpeg')
       File.exist?('testfile.jpeg').should be_true
     end
-    it "should return the file" do
+    it 'should return the file' do
       Spwn.dir('hello') do
         Spwn.file('testfile.jpeg')
       end
       Lazy.file('hello/testfile.jpeg').should be_an_instance_of Lazy::File
     end
-    it "should open the file when a block is given" do
+    it 'should open the file when a block is given' do
       Spwn.file('testfile.jpeg') do |file|
         file.closed?.should be_false
       end
